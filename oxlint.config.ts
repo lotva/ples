38dd36207f139fcf3583a41e25f6b48205c204d4
lotva <dlotva@gmail.com>
2026-09-01 23:52:48 +0300

    init

---
+++ oxlint.config.ts
@@ -0,0 +1,9 @@
+import loguxOxlintConfig from '@logux/oxc-configs/lint'
+import { defineConfig } from 'oxlint'
+
+export default defineConfig({
+  extends: [loguxOxlintConfig],
+  env: {
+    browser: true
+  }
+})
