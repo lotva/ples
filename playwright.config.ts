cdff99e0b80f9ea5afaa1330bbd7b1947c8c72ea
lotva <dlotva@gmail.com>
2026-09-02 23:52:28 +0300

    ci: add browser and integrations test workflows

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
-  reporter: 'list',
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI ? [['list'], ['html', { open: 'never' }]] : 'list',
   use: {
     baseURL: 'http://localhost:4173',
     trace: 'on-first-retry'
